6fee4354f2a05e8ed0ee69936ab70ba0d880108b
artem.mardakhaev2004 <artem.mardakhaev2004@gmail.com>
2024-09-29 12:55:24 -0400

    new migrations

---
+++ Billing.Persistence/Migrations/20240929165115_AddTotalPriceToBill.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Billing.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTotalPriceToBill : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "TotalPrice",
+                table: "Bills",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalPrice",
+                table: "Bills");
+        }
+    }
+}
